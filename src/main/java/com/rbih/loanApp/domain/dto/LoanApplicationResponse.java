1a5762fcf235621e5bd216b9e18f1060fe4232cd
Abhishek Singh <abhicse108@gmail.com>
2026-03-20 14:11:15 +0530

    Added changes to omit null fields

--- src/main/java/com/rbih/loanApp/domain/dto/LoanApplicationResponse.java
+++ src/main/java/com/rbih/loanApp/domain/dto/LoanApplicationResponse.java
@@ -5,10 +5,12 @@ import com.rbih.loanApp.domain.enums.RejectionReason;
 import com.rbih.loanApp.domain.enums.RiskBand;
 import lombok.Builder;
 import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.List;
 import java.util.UUID;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 @Builder
 public class LoanApplicationResponse {
